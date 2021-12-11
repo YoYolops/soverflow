99ff5b7c861272d8c8dba1099f1341a66e6ff109
Yohan Lopes <yohanhxh@gmail.com>
2021-12-11 13:02:03 -0300

    Creating database

---
+++ src/database/dump.sql
@@ -0,0 +1,78 @@
+CREATE TABLE "tags" (
+	"id" serial NOT NULL,
+	"name" TEXT NOT NULL UNIQUE,
+	CONSTRAINT "tags_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "questions" (
+	"id" serial NOT NULL,
+	"user_id" int NOT NULL,
+	"question" TEXT NOT NULL,
+	"submit_at" DATE NOT NULL DEFAULT 'NOW()',
+	"answer_id" int,
+	CONSTRAINT "questions_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "tags_group" (
+	"id" serial NOT NULL,
+	"tag_id" int NOT NULL,
+	"question_id" int NOT NULL,
+	CONSTRAINT "tags_group_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "answers" (
+	"id" serial NOT NULL,
+	"answer" TEXT NOT NULL,
+	"submit_at" DATE NOT NULL DEFAULT 'NOW()',
+	"submit_by" int NOT NULL,
+	CONSTRAINT "answers_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "users" (
+	"id" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	"token" uuid NOT NULL,
+	"class_id" int NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "classes" (
+	"id" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	CONSTRAINT "classes_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "questions" ADD CONSTRAINT "questions_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE "questions" ADD CONSTRAINT "questions_fk1" FOREIGN KEY ("answer_id") REFERENCES "answers"("id");
+
+ALTER TABLE "tags_group" ADD CONSTRAINT "tags_group_fk0" FOREIGN KEY ("tag_id") REFERENCES "tags"("id");
+ALTER TABLE "tags_group" ADD CONSTRAINT "tags_group_fk1" FOREIGN KEY ("question_id") REFERENCES "questions"("id");
+
+ALTER TABLE "answers" ADD CONSTRAINT "answers_fk0" FOREIGN KEY ("submit_by") REFERENCES "users"("id");
+
+ALTER TABLE "users" ADD CONSTRAINT "users_fk0" FOREIGN KEY ("class_id") REFERENCES "classes"("id");
